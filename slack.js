7835ae9b804fdc90afc82b27e35dab54e056bb8e
Yusuf Ibrohim <yusufikeolapo2002@gmail.com>
2025-02-13 23:45:31 +0100

    slack-replica

---
+++ slack.js
@@ -0,0 +1,55 @@
+// const mediaQuery = window.matchMedia("(max-width: 768px)");
+//         let card1 = document.querySelector(".card1");
+//         let card2 = document.querySelector(".card2");
+//         let card3 = document.querySelector(".card3");
+//         let card4 = document.querySelector(".card4");
+        
+
+//         // card1.classList.add("carddisplay");
+//         // card2.classList.add("carddisplay");
+//         // card3.classList.add("carddisplay");
+//         // card4.classList.add("carddisplay");
+//         // let randomNum = Math.floor(Math.random()*4);
+//         // alert(randomNum)
+//         let i = 0;
+//         let eachArr=["card1", "card2", "card3", "card4"];
+        
+        
+
+//         setInterval(function(){
+//             if(mediaQuery.matches){
+//                 //   card1.style.display = "none";
+//                     card1.classList.add("carddisplay");
+//                     card2.classList.add("carddisplay");
+//                     card3.classList.add("carddisplay");
+//                     card4.classList.add("carddisplay");
+//                     // setInterval(function(){
+//                     //         // alert("po")
+//                     //     }, 2000)
+
+//                     if(i = 0){
+//                         card1.classList.remove("carddisplay");
+//                         card2.classList.add("carddisplay");
+//                         card3.classList.add("carddisplay");
+//                         card4.classList.add("carddisplay");
+//                     }else if(i = 1){
+//                         card1.classList.add("carddisplay");
+//                         card2.classList.remove("carddisplay");
+//                         card3.classList.add("carddisplay");
+//                         card4.classList.add("carddisplay");
+//                     }else if(i = 2){
+//                         card1.classList.add("carddisplay");
+//                         card2.classList.add("carddisplay");
+//                         card3.classList.remove("carddisplay");
+//                         card4.classList.add("carddisplay");
+//                     }else if(i = 3){
+//                         card1.classList.add("carddisplay");
+//                         card2.classList.add("carddisplay");
+//                         card3.classList.add("carddisplay");
+//                         card4.classList.remove("carddisplay");
+//                     }
+//                     i++; 
+//                 }
+                
+//         }, 1000)
+        
